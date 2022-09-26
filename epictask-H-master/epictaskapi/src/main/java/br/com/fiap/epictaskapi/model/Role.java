155a1c8fd45061f33029a9d2be046893c95a04c2
MrMnzs <andremnzs@gmail.com>
2022-09-26 20:55:38 -0300

    alteração de usuario do BD

---
+++ epictask-H-master/epictaskapi/src/main/java/br/com/fiap/epictaskapi/model/Role.java
@@ -0,0 +1,36 @@
+package br.com.fiap.epictaskapi.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.security.core.GrantedAuthority;
+
+@Entity
+@Table(name="TB_ROLE")
+public class Role implements GrantedAuthority {
+
+    @Id @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String name;
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getAuthority() {
+        return this.name;
+    }
+
+}
